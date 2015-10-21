499d9f9345300e275a7c9a938858004a3ba1c029
ftsan <ftsan12345@gmail.com>
2015-10-21 17:43:11 +0900

    Use method reference

--- src/main/java/java8code/ch1/practice/Q2.java
+++ src/main/java/java8code/ch1/practice/Q2.java
@@ -2,6 +2,7 @@ package java8code.ch1.practice;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -12,6 +13,7 @@ public class Q2 {
     public static void main(String[] args) {
         File f = new File(System.getProperty("user.dir"));
         System.out.println(recursiveDirLists(f));
+        System.out.println(recursiveDirLists2(f));
     }
 
     private static List<File> recursiveDirLists(File file) {
@@ -21,6 +23,16 @@ public class Q2 {
                 dirNames.add(f);
                 dirNames.addAll(recursiveDirLists(f));
             }
+        });
+        return dirNames;
+    }
+
+    private static List<File> recursiveDirLists2(File... files) {
+        List<File> dirNames = new ArrayList<>();
+        Stream.of(files).forEach(f -> {
+            File[] subDirs = f.listFiles(File::isDirectory);
+            dirNames.add(f);
+            dirNames.addAll(recursiveDirLists2(subDirs));
         });
         return dirNames;
     }
