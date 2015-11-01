34d5e97d5d6bc964518ddd50b76bd1826ef66535
ftsan <ftsan12345@gmail.com>
2015-11-01 14:25:08 +0900

    ラムダ式を２つ受け取り、例外処理も可能な非同期で実行されるメソッドを実装

---
+++ src/main/java/java8code/ch3/Q16.java
@@ -0,0 +1,52 @@
+package java8code.ch3;
+
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+/**
+ * Created by futeshi on 2015/10/31.
+ */
+public class Q16 {
+    public static void main(String[] args) {
+        doInOrderAsync(
+                ()     -> {
+                    throw new RuntimeException("error");
+                },
+                (s, t) -> {
+                    if (t != null) {
+                        System.err.println(t.getMessage());
+                    } else {
+                        System.out.println(s);
+                    }
+                });
+    }
+
+    public static <T> void doInOrderAsync(Supplier<T> first, BiConsumer<T, Throwable> second) {
+        Thread thread = new Thread(() -> {
+            T result = null;
+            try {
+                result = first.get();
+                second.accept(result, null);
+            } catch (Throwable t) {
+                second.accept(result, t);
+            }
+        });
+        thread.start();
+    }
+
+
+    public static <T> void doInOrderAsync(
+            Supplier<T> first, Consumer<T> second, Consumer<Throwable> handler) {
+         Thread t = new Thread(() -> {
+             try {
+                 T result = first.get();
+                 second.accept(result);
+             } catch (Throwable throwable) {
+                 handler.accept(throwable);
+             }
+         });
+        t.start();
+    }
+}
